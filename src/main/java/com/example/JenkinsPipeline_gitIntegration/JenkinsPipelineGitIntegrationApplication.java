4f5c4b795b4d33065553ada50b03f05b9b8c6e71
KoushalKumar22 <koushalkumar2224@gmail.com>
2024-09-11 23:49:48 +0530

    testing

---
+++ src/main/java/com/example/JenkinsPipeline_gitIntegration/JenkinsPipelineGitIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.example.JenkinsPipeline_gitIntegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsPipelineGitIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsPipelineGitIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/JenkinsPipeline_gitIntegration/JenkinsPipelineGitIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JenkinsPipeline_gitIntegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsPipelineGitIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
